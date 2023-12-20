ae0d0b3fcaa46879461e740295ec73540b9bc216
danielecarpentiero <carpentierodaniele@gmail.com>
2023-12-20 11:55:56 +0100

    feat: second section

--- src/components/MainSecond.vue
+++ src/components/MainSecond.vue
@@ -1,9 +1,17 @@
 <template>
   <h6>corporate & weddings</h6>
   <h2>Baking Special Moments</h2>
-  <div class="photoshows">
-    <div class="show" v-for="ad in ads">
-      <img :src="ad.image" alt="" />
+  <div class="photoshows d-flex">
+    <div class="show mx-5" v-for="ad in ads">
+      <div class="info">
+        <h6 class="">{{ ad.preTitle }}</h6>
+        <h2 class="">{{ ad.title }}</h2>
+        <button class="d-flex align-items-center primary mt-2 px-3 py-2">
+          {{ ad.button }}
+        </button>
+      </div>
+      <img class="front" :src="ad.image" alt="" />
+      <img class="back" :src="ad.imageHover" alt="" />
     </div>
   </div>
 </template>
@@ -15,15 +23,15 @@ export default {
     return {
       ads: [
         {
-          image: "../assets/img/corporate-bg.jpg",
-          imageHover: "../assets/img/corporate-hover-bg.jpg",
+          image: "src/assets/img/corporate-bg.jpg",
+          imageHover: "src/assets/img/corporate-hover-bg.jpg",
           preTitle: "international bakery",
           title: "Corporate Events",
           button: "Explore more",
         },
         {
-          image: "../assets/img/wedding-bg.jpg",
-          imageHover: "../assets/img/wedding-hover-bg.jpg",
+          image: "src/assets/img/wedding-bg.jpg",
+          imageHover: "src/assets/img/wedding-hover-bg.jpg",
           preTitle: "private dinings hall",
           title: "Wedding & Parties",
           button: "Explore more",
@@ -34,18 +42,55 @@ export default {
 };
 </script>
 
-<style scoped>
-img {
-  width: 100px;
-  height: 100px;
+<style scoped lang="scss">
+button {
+  margin: 0 auto;
 }
+.photoshows {
+  width: 100%;
+  padding: 50px 200px;
+  .show {
+    position: relative;
+    .info {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      opacity: 0;
+      transition: opacity 0.3s ease;
+      z-index: 1;
+    }
 
+    .back {
+      opacity: 0;
+      position: absolute;
+      top: 0;
+      left: 0;
+      transition: opacity 0.3s ease;
+      z-index: 0;
+    }
+    &:hover {
+      .front {
+        opacity: 0;
+      }
+      .back {
+        opacity: 1;
+      }
+      .info {
+        opacity: 1;
+      }
+    }
+  }
+  img {
+    width: 100%;
+  }
+  h2,
+  h6 {
+    color: white;
+  }
+}
 h6,
 h2 {
   text-align: center;
-}
-
-h6 {
-  margin-top: 150px;
 }
 </style>
